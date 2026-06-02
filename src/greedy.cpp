1ddf04c45a0e93b895110f1402cc5334e41dd606
lynnzyao <lynn.z.yao@gmail.com>
2026-06-02 17:49:11 +0800

    update

--- src/greedy.cpp
+++ src/greedy.cpp
@@ -15,3 +15,34 @@ bool asteroids_destroyed(int mass, std::vector<int>& asteroids) {
   }
   return true;
 }
+
+
+// 2025.06.02
+// https://leetcode.com/problems/earliest-finish-time-for-land-and-water-rides-i/
+int earliest_finish_time(
+  std::vector<int>& lst, std::vector<int>& ld, 
+  std::vector<int>& wst, std::vector<int>& wd
+) {
+  int le = INT_MAX, we = INT_MAX, lsm = INT_MAX, wsm = INT_MAX;
+  for (int i = 0; i < lst.size(); ++i) {
+    le = std::min(le, lst[i] + ld[i]);
+  }
+  for (int i = 0; i < wst.size(); ++i) {
+    we = std::min(we, wst[i] + wd[i]);
+  }
+  for (int i = 0; i < wst.size(); ++i) {
+    if (wst[i] < le) {
+      lsm = std::min(lsm, le + wd[i]);
+    } else {
+      lsm = std::min(lsm, wst[i] + wd[i]);
+    }
+  }
+  for (int i = 0; i < lst.size(); ++i) {
+    if (lst[i] < we) {
+      wsm = std::min(wsm, we + ld[i]);
+    } else {
+      wsm = std::min(wsm, lst[i] + ld[i]);
+    }
+  }
+  return std::min(lsm, wsm);
+}
